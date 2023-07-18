f0a2eac58a1c4652437a964198792071264b0e7d
Raquel Pedroso <82239960+raquelspedroso@users.noreply.github.com>
2023-07-18 13:39:07 -0300

    Add files via upload

---
+++ trabalho aws/teste.js
@@ -0,0 +1,28 @@
+function mostrarIntegrantes() {
+    var integrantes = document.getElementById("Integrantes do Grupo 5 Turma 11 ESCOLA DA NUVEM ");
+    if (integrantes.style.display === "none") {
+        integrantes.style.display = "block";
+    } else {
+        integrantes.style.display = "none";
+    }
+  }
+
+
+
+
+else if (member === 'noisss')
+    modalTitle.textContent = 'GRUPO 5 AWS ESCOLA DA NUVEM';
+    modalImage.src = 'noisss.jpg';
+    modalContent.innerHTML = `
+    <p>Cloud AWS - Escola da Nuvem </p>
+    <h3>GRUPO 5</h3>
+    <h3>Membros</h3>
+    <ul>
+    <li>Moises Ximenes</li>
+    <li>Jefferson Silva</li>
+    <li>Wellington Souza</li>
+    <li>Cassiano Lira</li>
+    <li>Fabiola Leles</li>
+    <li>Raquel Pedroso</li>
+    <li>Tiago Ribeiro</li>
+    </ul>
